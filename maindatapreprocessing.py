06e9d1396f0b3795f56618059899c82ca1cbfcef
Doniele Arys Antonio <arysantonio363@gmail.com>
2025-01-18 19:19:41 +0800

    added: initial data overview

--- maindatapreprocessing.py
+++ maindatapreprocessing.py
@@ -3,3 +3,10 @@ from scipy import stats
 
 df = pd.read_csv('assets.train.csv')
 
+print('Dataset Overview:')
+print(df.head(10))
+print(df.tail(10))
+print('\nDataset Summary:')
+print(df.describe())
+print(df.info())
+
