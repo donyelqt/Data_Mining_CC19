d3312644524ace5b427edee2402e65f474ad299d
Doniele Arys Antonio <arysantonio363@gmail.com>
2025-01-18 19:24:38 +0800

    added: filling missed values

--- maindatapreprocessing.py
+++ maindatapreprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from scipy import stats
 
-df = pd.read_csv('assets.train.csv')
+df = pd.read_csv('assets/train.csv')
 
 print('Dataset Overview:')
 print(df.head(10))
@@ -12,3 +12,6 @@ print(df.info())
 
 print('\nMissing Values Count:')
 print(df.isnull().sum())
+
+df['Age'] = df['Age'].fillna(df['Age'].mean())
+df['Embarked'] = df['Embarked'].fillna(df['Embarked'].mode()[0])
